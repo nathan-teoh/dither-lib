47c1acaf7eb5b908ea1c2201f413daee75d4fba6
Nathan Teoh <me@nathanteoh.com>
2024-10-07 23:46:36 -0700

    more refactoring

--- src/dither.rs
+++ src/dither.rs
@@ -1,14 +1,10 @@
-use image::{imageops::FilterType, DynamicImage};
+use image::{imageops::FilterType, DynamicImage, Rgb};
 use ndarray::{arr2, concatenate, Array2, Axis};
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 
 const R_CHANNEL_MULTIPLIER: f64 = 0.2126;
 const G_CHANNEL_MULTIPLIER: f64 = 0.7152;
 const B_CHANNEL_MULTIPLIER: f64 = 0.0722;
-
-pub struct DitherImage {
-    pub image: DynamicImage,
-}
 
 pub struct DitherBuilder {
     image: DynamicImage,
@@ -18,14 +14,14 @@ pub struct DitherBuilder {
     shadows: (u8, u8, u8),
     highlights: (u8, u8, u8),
 }
-impl DitherImage {
+impl DitherBuilder {
     pub fn new(image: DynamicImage) -> DitherBuilder {
         let width = image.width();
         let height = image.height();
         DitherBuilder {
             image,
-            width: width,
-            height: height,
+            width,
+            height,
             shadows: (0, 0, 0),
             highlights: (255, 255, 255),
             level: 2,
@@ -59,8 +55,8 @@ impl DitherBuilder {
         self.shadows = shadows;
         self
     }
-    // Generate a dithered image given a set of parameters
-    pub fn generate(self) -> DitherImage {
+    // Generate a dithered image given a set of parameters and returns a DynamicImage
+    pub fn generate(self) -> DynamicImage {
         //generate equalizer
         let num = 2_u8.pow(self.level.into());
         let equalizer = 1. / (num as f32).powf(2.);
@@ -88,13 +84,9 @@ impl DitherBuilder {
 
                 let pixel_brightness = gamma_correct(pixel_brightness);
                 if pixel_brightness > (1. - bayer_layer[[y, x]]).into() {
-                    pixel[0] = self.highlights.0;
-                    pixel[1] = self.highlights.1;
-                    pixel[2] = self.highlights.2;
+                    set_pixel(*pixel, self.highlights);
                 } else {
-                    pixel[0] = self.shadows.0;
-                    pixel[1] = self.shadows.1;
-                    pixel[2] = self.shadows.2;
+                    set_pixel(*pixel, self.shadows);
                 }
             });
 
@@ -102,10 +94,14 @@ impl DitherBuilder {
         for (x, y, pixel) in image_buffer {
             new_img_buffer.put_pixel(x, y, *pixel);
         }
-        DitherImage {
-            image: DynamicImage::ImageRgb8(new_img_buffer),
-        }
+        DynamicImage::ImageRgb8(new_img_buffer)
     }
+}
+
+fn set_pixel(pixel: &mut Rgb<u8>, color: (u8, u8, u8)) {
+    pixel[0] = color.0;
+    pixel[1] = color.1;
+    pixel[2] = color.2;
 }
 
 fn generate_bayer(level: u8) -> Array2<i32> {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use dither::DitherImage;
-use image::{DynamicImage, ImageReader};
+use dither::DitherBuilder;
+use image::ImageReader;
 mod dither;
 fn main() {
     let filename = "input.jpg";
 
     let image_file = ImageReader::open(filename).unwrap();
     let image_file = image_file.decode().unwrap();
-    let dither_image = DitherImage::new(image_file)
+    let dither_image = DitherBuilder::new(image_file)
         .highlights((255, 255, 255))
         .shadows((0, 0, 0))
         .generate();
-    dither_image.image.save("output.jpg").unwrap();
+    dither_image.save("output.jpg").unwrap();
 }
